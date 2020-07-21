9c331041efe9ab652696b9116456e722fad68973
rhw <ruohanw89@gmail.com>
2020-07-20 18:27:29 -0600

    generate README

--- dist/README.md
+++ dist/README.md
@@ -1,1 +1,48 @@
-# werw
+# README Generator
+
+## Description
+
+Fenerating reame markdown file per answers from command-line prompt
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [License](#license)
+- [Badges](#badges)
+- [GitHub](#github)
+- [Contact](#contact)
+- [Tests](#tests)
+
+## Installation
+
+You just need to git clone this repo to your local drive and start to use it.
+
+## Usage
+
+Generate README.md file for your next project / applicaiton.
+
+## Contribution
+
+Made with ❤️ by Ruohan
+
+## License
+
+MIT License
+
+## Badges
+
+![badmath](https://img.shields.io/conda/l/conda-forge/setuptools?color=Blue&label=License&logo=MIT License&logoColor=blue&style=plastic)
+
+## GitHub
+
+My GitHub link: (https://github.com/rhw-git)
+
+## Contact
+
+You can reach me by email: (ruohanw89@gmail.com)
+
+## Tests
+
+test pending
--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -3,9 +3,68 @@ const fs = require("fs");
 
 // function to generate markdown for README
 function generateMarkdown(data) {
+  console.log(data);
+  // deconstruct data
+  const {
+    title,
+    description,
+    installation,
+    usage,
+    contribution,
+    test,
+    license,
+    gitName,
+    email,
+  } = data;
   return `
-  # ${data.title}
+  # ${title}
 
+  ##  Description
+
+  ${description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [License](#license)
+  - [Badges](#badges)
+  - [GitHub](#github)
+  - [Contact](#contact)
+  - [Tests](#tests)
+
+  ## Installation
+
+  ${installation}
+
+  ## Usage
+
+  ${usage}
+
+  ## Contribution
+
+  ${contribution}
+
+  ## License
+
+  ${license}
+
+  ## Badges
+
+  [github-languages-image]: ![Conda - License](https://img.shields.io/conda/l/conda-forge/setuptools?color=Blue&label=License&logo=${license}&logoColor=blue&style=plastic)
+
+  ## GitHub
+
+  My GitHub link: (https://github.com/${gitName})
+
+  ## Contact
+
+  You can reach me by email: (${email})
+
+  ## Tests
+
+  ${test}
   `;
 }
 
